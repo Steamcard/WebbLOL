fbe6b42b3387c66479de32b5eee8380648e176a7
Kasper Falk <kasfal0128@skynet.halmstad.se>
2020-03-12 13:08:27 +0100

    hej

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const mongoId = require('mongodb').ObjectID;
 const login = require("./Login/login")
 const auth = require("./Login/auth")
 const request = require('request');
-const apiKey = 'RGAPI-b233b4b8-ab72-41ad-89d5-ef5d681cfb11';
+const apiKey = 'RGAPI-616d561e-c64e-4601-adec-5d5e9994c66d';
 
 
 module.exports = async function(app){
@@ -12,29 +12,46 @@ app.get("/",function(req,res){
     
 });
 
+//GEr dig stats om din lol gubbe
 app.get("/stats",function(req,res)
 {
-    let options = {
-        url: 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/16FPS?api_key=RGAPI-b233b4b8-ab72-41ad-89d5-ef5d681cfb11',
+    let SummonerInfo = {
+        url: `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/16FPS?api_key=${apiKey}`,
         json: true,
         method: "get"
     }
 
-    let top = {
-        url: 'https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/KIVgOiuLCgFCh85bIDODTztdP28dawk5CDOQEU4NlFQiIXk?api_key=RGAPI-b233b4b8-ab72-41ad-89d5-ef5d681cfb11',
-        json: true,
-        method: "get"
-    }
     
-    request(top, (err,res, matches)=>{
-        request(options, (error,response, user)=>{
-            //res.render('stats', {body:body,hejsan:hejsan});
-            console.log(user);
-            //console.log(body);
-            res.render('stats', user);
-        });
-        
-    })
+    request(SummonerInfo, (error,response, user)=>{
+
+        let Match = {
+            url: `https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/${user.accountId}?api_key=${apiKey}`,
+            json: true,
+            method: "get"
+        }
+
+        console.log(user);
+        console.log(Match);
+            
+            request(Match, (serr,ses, macther)=>{
+
+                let entries = {
+                    url: `https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/${user.id}?api_key=${apiKey}`,
+                    json: true,
+                    method: "get"
+                }
+
+                    request(entries, (error3,response3, entrieses)=>{
+                    console.log(entrieses);
+
+                //Tar de sisa arrayen på macthes så inte alla loggar (över 100st)//
+                last = macther.matches.slice(0,4);
+                
+                res.render('stats', {user: user, matches: last, entrieses:entrieses});
+            });
+            
+        })
+        })
 });
 
     
