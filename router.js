4870953bec53e9bf65208bbf8ea189cbb7ae33ba
Kasper Falk <kasfal0128@skynet.halmstad.se>
2020-03-26 01:24:54 +0100

    yo

--- router.js
+++ router.js
@@ -4,7 +4,7 @@ const auth = require("./Login/auth")
 const request = require('request');
 
 //Hämtar APIKEY Från LOLAPI hemsidan | Måste hämtas ny varje 24 tim!
-const apiKey = 'RGAPI-efcb7e6d-c44f-40a7-be0b-0696dd4f0226';
+const apiKey = 'RGAPI-fea142f5-aecd-42e9-a2d8-80ec842ab94c';
 
 
 module.exports = async function(app){
@@ -15,12 +15,13 @@ app.get("/",function(req,res){
 
     
 });
-
+//funkar ej atm med try catch
+try{
 //Ger dig stats om din lol gubbe
 app.post("/stats",function(req,res)
-{
+
+ {
     let name = JSON.parse(JSON.stringify(req.body)).Summoner;
-    console.log(name);
 
     let SummonerInfo = {
         url: `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${apiKey}`,
@@ -49,16 +50,63 @@ app.post("/stats",function(req,res)
 
                     request(entries, (error3,response3, entrieses)=>{
 
+
+
+
+
                 //Tar de sisa arrayen på macthes så inte alla loggar (över 100st)
                 last = macther.matches.slice(0,4);
                 
-                
-                res.render('stats', {user: user, matches: last, entrieses:entrieses});
+                //forEach loop där element.champion hämtar ut alla last id och lägger in i getChampName
+                last.forEach(element => 
+                    {
+                        //Hämtar champion id i arreyen last
+                        getChampName(element.champion);
+
+                    //Funktionen hämtar ut Champion namnen med hjälp av id i arryen last
+                    function getChampName(id) {
+
+                    //Request metoden går in i en JSON fil med över 144 champions som inehåller mängder med olika data
+                    request('http://ddragon.leagueoflegends.com/cdn/10.6.1/data/en_US/champion.json', function (error, response, body) {
+
+                        //Parsar bodyn
+                        const list = JSON.parse(body);
+
+                        //hämtar datan till championList
+                        const championList = list.data;
+
+                        //For-Loop som går igenom varje Champion
+                        for (const i in championList) {
+                        //Stämmer 1 av Champion listans id överens med ett av id i last så läggs Champion namnets data från championList till arrayn för att sedan skicka in till stats
+                        if (championList[i].key == id) {
+                            const arr = [championList[i].id];
+                            console.log(arr);
+                        }
+
+                        //console.log(championList[i].id + " | " + championList[i].key);
+                        }
+
+                    });
+                    }
+                    });
+
+                    //något sätt att få ut arryen? (arr)
+                    console.log(arr);
+                    res.render('stats', {user: user, matches: last, entrieses:entrieses, style:'<link rel="stylesheet" href="/css/stats.css"> <link rel="stylesheet" href="/css/header.css">'});
             });
             
         })
         })
 });
+}
+catch(error){
+    app.get("/",function(req,res){
+
+        res.render('home',{style:'<link rel="stylesheet" href="/css/home.css"> <link rel="stylesheet" href="/css/header.css">'});
+});
+}
+
+
 
     
 app.post("/login", login)
